a6ba7de04bec7fd37dbd82508481b6bd1b343c60
Jenny Schutzman <jennyschutzman@gmail.com>
2021-09-26 12:16:10 -0400

    set up config, form, results card, results page, routing, styling.  Still need to format time zones from UTS

---
+++ src/components/ResultsCard/index.js
@@ -0,0 +1,80 @@
+import React from "react";
+
+const ResultsCard = (data) => {
+  const item = data.data;
+
+  return (
+    <>
+      {item && (
+        <div className="p-5 bg-green-500 rounded-lg m-4">
+          <h3 className="text-xl leading-6 font-medium text-white">
+            {item.location.toUpperCase()}
+          </h3>
+
+          <dl className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3">
+            <div className="relative bg-white px-4 py-5 sm:py-6 sm:px-6 shadow rounded-lg overflow-hidden">
+              <dt>
+                <div className="absolute bg-indigo-500 rounded-md p-3">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6 text-white"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+                    />
+                  </svg>
+                </div>
+                <p className="ml-16 text-sm font-medium text-gray-500 truncate">
+                  Sunrise
+                </p>
+              </dt>
+              <dd className="ml-16 pb-6 flex items-baseline sm:pb-7">
+                <p className="text-2xl font-semibold text-gray-900">
+                  {item.sunrise}
+                </p>
+              </dd>
+            </div>
+
+            <div className="relative bg-white pt-5 px-4 py-5 sm:py-6 sm:px-6 shadow rounded-lg overflow-hidden">
+              <dt>
+                <div className="absolute bg-indigo-500 rounded-md p-3">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6 text-white"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+                    />
+                  </svg>
+                </div>
+                <p className="ml-16 text-sm font-medium text-gray-500 truncate">
+                  Sunset
+                </p>
+              </dt>
+              <dd className="ml-16 pb-6 flex items-baseline sm:pb-7">
+                <p className="text-2xl font-semibold text-gray-900">
+                  {item.sunset}
+                </p>
+              </dd>
+            </div>
+          </dl>
+        </div>
+      )}
+    </>
+
+  );
+};
+
+export default ResultsCard;
---
+++ src/pages/Results/index.js
@@ -0,0 +1,124 @@
+import React, { useEffect, useState } from "react";
+import ResultsCard from "../../components/ResultsCard";
+import { useLocation } from "react-router-dom";
+import moment from 'moment'
+
+
+export default function Results() {
+    const [dataForApi, setDataForApi] = useState([]);
+    const [dataForCards, setDataForCards] = useState();
+    const location = useLocation();
+
+    useEffect(() => {
+        if (location.state !== []) {
+            const data = [
+                {
+                    id: 1,
+                    location: `${location.state.city1}`,
+                    lat: `${location.state.lat1}`,
+                    long: `${location.state.long1}`,
+                },
+                {
+                    id: 2,
+                    location: `${location.state.city2}`,
+                    lat: `${location.state.lat2}`,
+                    long: `${location.state.long2}`,
+                },
+                {
+                    id: 3,
+                    location: `${location.state.city3}`,
+                    lat: `${location.state.lat3}`,
+                    long: `${location.state.long3}`,
+                },
+                {
+                    id: 4,
+                    location: `${location.state.city4}`,
+                    lat: `${location.state.lat4}`,
+                    long: `${location.state.long4}`,
+                },
+                {
+                    id: 5,
+                    location: `${location.state.city5}`,
+                    lat: `${location.state.lat5}`,
+                    long: `${location.state.long5}`,
+                },
+            ];
+            setDataForApi(data);
+        }
+    }, [location.state]);
+
+    useEffect(() => {
+        if (dataForApi.length !== 0) {
+            const getAPIdata = async (passUpData) => {
+                console.log("passUpData", passUpData)
+                try {
+                    let response = await fetch(
+                        `https://api.sunrise-sunset.org/json?lat=${passUpData.lat}&lng=${passUpData.long}`
+                    );
+                    const apiResponse = await response.json();
+                    if (apiResponse.results.sunrise && apiResponse.status === "OK") {
+                        const sunrise = apiResponse.results.sunrise
+                        const sunset = apiResponse.results.sunset
+                        const data = dataForApi.filter(item => item.id === passUpData.id)
+                        const locationSunriseSunset = data.map((item) => {
+                            return {
+                                location: item.location,
+                                sunrise: sunrise,
+                                sunset: sunset,
+                                id: passUpData.id,
+                            };
+                        })
+                        return locationSunriseSunset;
+                    }
+                } catch (err) {
+                    console.log(err);
+                }
+            };
+            let newArray;
+            getAPIdata(dataForApi[0])
+                .then((res) => {
+                    newArray = [...(res)]
+                })
+                .then(() => {
+                    getAPIdata(dataForApi[1])
+                        .then((res) => {
+                            newArray = newArray.concat(...res)
+                        })
+                        .then(() => {
+                            getAPIdata(dataForApi[2])
+                                .then((res) => {
+                                    newArray = newArray.concat(...res)
+                                })
+                                .then(() =>
+                                    getAPIdata(dataForApi[3])
+                                        .then((res) => {
+                                            newArray = newArray.concat(...res)
+                                        })
+                                        .then(() =>
+                                            getAPIdata(dataForApi[4])
+                                                .then((res) => {
+                                                    newArray = newArray.concat(...res)
+                                                    setDataForCards(newArray)
+                                                })
+                                        )
+                                )
+                        })
+
+                })
+        }
+    }, [dataForApi, location.state]);
+
+    console.log("dataForCards", dataForCards);
+
+    return (
+        <>
+            {dataForCards &&
+                (
+                    dataForCards.map((item) => {
+                        return <ResultsCard data={item} key={item} />;
+                    })
+                )
+            }
+        </>
+    );
+}
